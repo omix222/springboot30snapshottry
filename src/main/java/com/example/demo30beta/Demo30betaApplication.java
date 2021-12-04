3d20833015a54ee1ef686e2ed885d827b82a309a
Hiromi Takahashi <hiromix222@gmail.com>
2021-12-04 19:15:35 +0900

    first commit

---
+++ src/main/java/com/example/demo30beta/Demo30betaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo30beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo30betaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo30betaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo30beta/SampleRestController.java
@@ -0,0 +1,28 @@
+package com.example.demo30beta;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/memo")
+public class SampleRestController {
+    class Book{
+        private String name;
+        private int price;
+        public Book(String name,int price) {
+            this.name = name;
+            this.price = price;
+        }
+        public String getName(){
+            return name;
+        }
+        public int getPrice(){
+            return price;
+        }
+    }
+    @GetMapping
+    public Book getBook(){
+        return new Book("やさしいJava",2580);
+    }
+}
