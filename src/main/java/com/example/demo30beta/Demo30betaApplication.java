83e360c7e58f454da28726745a83a9160dcb89b4
Hiromi Takahashi <hiromix222@gmail.com>
2022-02-11 08:03:58 +0900

    first commit

--- src/main/java/com/example/demo30beta/Demo30betaApplication.java
+++ src/main/java/com/example/demo30beta/Demo30betaApplication.java
@@ -3,11 +3,16 @@ package com.example.demo30beta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.function.Function;
+
 @SpringBootApplication
 public class Demo30betaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo30betaApplication.class, args);
-	}
+
+	    SpringApplication.run(Demo30betaApplication.class, args);
+
+    }
 
 }
--- src/main/java/com/example/demo30beta/SampleRestController.java
+++ src/main/java/com/example/demo30beta/SampleRestController.java
@@ -7,20 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/book")
 public class SampleRestController {
-    class Book{
-        private String name;
-        private int price;
-        public Book(String name,int price) {
-            this.name = name;
-            this.price = price;
-        }
-        public String getName(){
-            return name;
-        }
-        public int getPrice(){
-            return price;
-        }
-    }
     @GetMapping
     public Book getBook(){
         return new Book("やさしいJava",2580);
